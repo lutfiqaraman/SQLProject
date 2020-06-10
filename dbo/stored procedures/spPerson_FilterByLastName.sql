43146e0a92096fb68449a5e42dbb7f72bddb1971
lutfiqaraman <lutfiqaraman@gmail.com>
2020-06-10 21:52:09 +0300

    Add project files.

---
+++ dbo/stored procedures/spPerson_FilterByLastName.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spPerson_FilterByLastName]
+	@LastName nvarchar(50)
+AS
+
+BEGIN
+	select [Id], [FirstName], [LastName] 
+	from dbo.Person
+	where LastName = @LastName
+END
---
+++ dbo/views/FullPerson.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[FullPerson]
+	AS
+	
+	select [persontbl].[FirstName], [persontbl].[LastName], 
+		   [addresstbl].[StreetAddress], [addresstbl].[City], 
+		   [addresstbl].[State], [addresstbl].[ZipCode]
+	from dbo.Person persontbl
+	left join dbo.Address addresstbl
+	on persontbl.Id = addresstbl.PersonId
